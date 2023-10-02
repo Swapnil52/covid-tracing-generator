6f58b1850a2fba279fae495205080618fce83100
Swapnil Dhanwal <dhanwal@usc.edu>
2023-10-02 15:28:10 -0700

    Add meeting rooms

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
 from random import randint
+from inflection import underscore
 
 
 class MySQLInsertable:
@@ -8,9 +9,9 @@ class MySQLInsertable:
     __VALUE_TEMPLATE = "'%s'"
 
     def get_insert_query(self) -> str:
-        table_name = self.__get_class_name().lower()
+        table_name = underscore(self.__get_class_name())
         columns, values = self.__get_column_values()
-        query = ("INSERT INTO %s %s "
+        query = ("INSERT INTO `%s` %s "
                  "VALUES %s;") % (table_name, columns, values)
         return query
 
@@ -40,9 +41,10 @@ class MySQLInsertable:
 
 
 class Employee(MySQLInsertable):
-    def __init__(self, employee_id: int, name: str, office_number: int, floor_number: int, phone_number: str,
+
+    def __init__(self, _id: str, name: str, office_number: int, floor_number: int, phone_number: str,
                  email_id: str):
-        self.__id = employee_id
+        self.__id = _id
         self.__name = name
         self.__phone_number = phone_number
         self.__email_id = email_id
@@ -50,25 +52,30 @@ class Employee(MySQLInsertable):
         self.__floor_number = floor_number
 
 
+class MeetingRoom(MySQLInsertable):
+    def __init__(self, _id, floor_number):
+        self.__id = _id
+        self.__floor_number = floor_number
+
+
 class Generator:
     def __init__(self, first_names: list, last_names: list, max_rooms: int, max_floors: int):
-        self.__employee_idx = 0
-        self.__floor_idx = 0
-        self.__room_idx = 0
         self.__first_names = first_names
         self.__last_names = last_names
         self.__max_rooms = max_rooms
         self.__max_floors = max_floors
 
-    def employee(self) -> Employee:
-        _id = self.__employee_idx
+    def employee(self, _id) -> Employee:
         name = self.__generate_name()
         office_number = randint(1, self.__max_rooms)
-        floor_number = randint(0, self.__max_floors)
+        floor_number = randint(1, self.__max_floors)
         phone_number = self.__generate_number()
         email_id = self.__generate_email(name)
-        self.__employee_idx += 1
         return Employee(_id, name, office_number, floor_number, phone_number, email_id)
+
+    def meeting_room(self, _id):
+        floor_number = randint(1, self.__max_floors)
+        return MeetingRoom(_id, floor_number)
 
     def __generate_name(self) -> str:
         i = randint(0, len(self.__first_names) - 1)
@@ -98,21 +105,32 @@ class Generator:
 
 
 class Company:
-    def __init__(self, generator: Generator, max_employees: int):
+    def __init__(self, generator: Generator, max_employees: int, max_meeting_rooms: int):
         self.__generator = generator
         self.__max_employees = max_employees
+        self.max_meeting_rooms = max_meeting_rooms
         self.__employees = self.__generate_employees()
+        self.__meeting_rooms = self.__generate_meeting_rooms()
 
-    def dump_employees(self):
-        with open("./employee.sql", "w") as f:
-            for employee in self.__employees:
-                f.write("%s\n" % employee.get_insert_query())
+    def dump(self):
+        with open("./sql/employee.sql", "w") as f:
+            for meeting_room in self.__employees:
+                f.write("%s\n" % meeting_room.get_insert_query())
+        with open("./sql/meeting_room.sql", "w") as f:
+            for meeting_room in self.__meeting_rooms:
+                f.write("%s\n" % meeting_room.get_insert_query())
 
     def __generate_employees(self) -> list:
         employees = []
-        for i in range(self.__max_employees):
-            employees.append(self.__generator.employee())
+        for idx in range(self.__max_employees):
+            employees.append(self.__generator.employee(idx))
         return employees
+
+    def __generate_meeting_rooms(self):
+        meeting_rooms = []
+        for idx in range(self.__max_employees):
+            meeting_rooms.append(self.__generator.meeting_room(idx))
+        return meeting_rooms
 
     @property
     def employees(self):
@@ -123,8 +141,8 @@ def main():
     with open("./names.json", "r") as f:
         names = json.load(f)
     generator = Generator(names["first_names"], names["last_names"], 10, 10)
-    company = Company(generator, 50)
-    company.dump_employees()
+    company = Company(generator, 100, 20)
+    company.dump()
 
 
 if __name__ == '__main__':
